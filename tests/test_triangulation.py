389257966ba0f7d4141497dfde672334afd52859
samuelyvars <samuelyvars07@gmail.com>
2025-11-04 17:01:28 +0100

    Add unit tests for triangulation

---
+++ tests/test_triangulation.py
@@ -0,0 +1,56 @@
+import pytest
+
+class TestTriangulation:
+
+    def test_triangulate_three_points(self):
+        point_set = PointSet([
+            (0.0, 0.0),
+            (1.0, 0.0),
+            (0.0, 1.0),
+        ])
+        triangulator = Triangulator(point_set)
+
+        triangles = triangulator.triangulate()
+
+        assert len(triangles) == 1
+
+    def test_triangulate_four_points_square(self):
+        point_set = PointSet([
+            (0.0, 0.0),
+            (1.0, 0.0),
+            (1.0, 1.0),
+            (0.0, 1.0),
+        ])
+        triangulator = Triangulator(point_set)
+
+        triangles = triangulator.triangulate()
+
+        assert len(triangles) == 2
+
+    def test_triangulate_polygon(self):
+        point_set = PointSet([
+            (0.0, 0.0),
+            (2.0, 0.0),
+            (2.0, 2.0),
+            (1.0, 1.0),
+            (0.0, 2.0),
+        ])
+        triangulator = Triangulator(point_set)
+
+        triangles = triangulator.triangulate()
+        n = len(point_set)
+
+        assert len(triangles) == n - 2
+
+    def test_triangulate_less_3_points(self):
+        point_set = PointSet([
+            (0.0, 0.0),
+            (1.0, 0.0),
+        ])
+        triangulator = Triangulator(point_set)
+
+        triangles = triangulator.triangulate()
+
+        assert len(triangles) == 0
+
+
