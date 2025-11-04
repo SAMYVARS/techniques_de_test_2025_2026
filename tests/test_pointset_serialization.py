3f9764a0c8ccb0545ce7179ea222e56dec022fb8
samuelyvars <samuelyvars07@gmail.com>
2025-11-04 17:49:17 +0100

    Add unit tests for PointSet serialization

---
+++ tests/test_pointset_serialization.py
@@ -0,0 +1,44 @@
+import pytest
+
+class TestPointSetSerialization:
+
+    def test_serialize_empty_pointset(self):
+        point_set = PointSet([])
+
+        serialized = point_set.serialize()
+
+        assert len(serialized) == 4
+
+    def test_serialize_single_point(self):
+        point_set = PointSet([
+            (1.0, 2.0),
+        ])
+
+        serialized = point_set.serialize()
+
+        assert len(serialized) == 12
+
+    def test_serialize_multiple_points(self):
+        point_set = PointSet([
+            (0.0, 0.0),
+            (1.0, 1.0),
+            (2.0, 2.0),
+        ])
+
+        serialized = point_set.serialize()
+
+        assert len(serialized) == 28
+
+    def test_serialize_deserialize_pointset(self):
+        original_points = [
+            (0.0, 0.0),
+            (1.0, 0.0),
+            (0.0, 1.0),
+        ]
+        point_set = PointSet(original_points)
+
+        serialized = point_set.serialize()
+        deserialized = PointSet.deserialize(serialized)
+
+        assert len(deserialized) == len(point_set)
+
